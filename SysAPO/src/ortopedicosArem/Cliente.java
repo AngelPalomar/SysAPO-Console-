0a56f28b613dbc0c792c789e29ef4821e3d54698
angel <angel@PALOMAR>
2020-04-05 04:12:01 -0500

    Creada heredación de la clase Cliente de Usuario.

--- SysAPO/src/ortopedicosArem/Cliente.java
+++ SysAPO/src/ortopedicosArem/Cliente.java
@@ -2,8 +2,25 @@ package ortopedicosArem;
 
 public class Cliente extends Usuario {
 
-	public void agregarCliente() {
+	public Cliente() {
+		super();
+	}
 
+	public Cliente(int idUsuario, int tipoUsuario, String nombreUsuario, String apellidoUsuario, String contrasena,
+			String correoUsuario, String telefonoUsuario, String calle, String numeroInterior, String numeroExterior,
+			String cp, String colonia, String municipio, String estado) {
+		
+		super(idUsuario, tipoUsuario, nombreUsuario, apellidoUsuario, contrasena, correoUsuario, telefonoUsuario, calle,
+				numeroInterior, numeroExterior, cp, colonia, municipio, estado);
+		
+	}
+	
+	private void CargarCliente() {
+		
+	}
+
+	public void agregarCliente() {
+		
 	}
 
 	public void eliminarCliente() {
